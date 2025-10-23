e42932255cff3d0ce18eaabd28f085341e0e3279
Santiago Espinosa Giraldo <santiagoespinosagiraldo1@gmail.com>
2025-10-23 08:18:01 -0500

    Add Layout component for consistent page structure

---
+++ frontend/src/components/layout/Layout.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Box, Container } from '@mui/material';
+import Header from './Header';
+import Footer from './Footer';
+import { LayoutProps } from '../../types';
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+      }}
+    >
+      <Header />
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 3,
+        }}
+      >
+        <Container maxWidth="lg">
+          {children}
+        </Container>
+      </Box>
+      <Footer />
+    </Box>
+  );
+};
+
+export default Layout;
